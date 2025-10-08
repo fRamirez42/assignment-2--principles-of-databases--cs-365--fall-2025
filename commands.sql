76e4f0dd6e6028f3d67ee3ad50cce95232e6d42b
Foxyz24 <143655653+fRamirez42@users.noreply.github.com>
2025-10-08 09:43:07 -0400

    Add a new entry has been saved

--- commands.sql
+++ commands.sql
@@ -1,1 +1,16 @@
--- Write commands like this --
+-- Command 1: Add a new entry --
+INSERT INTO sites (url)
+VALUES ('https://snapchat.com');
+
+INSERT INTO accounts (site_ID, email, username)
+VALUES (LAST_INSERT_ID(), 'felipe@snapchat.com', 'feliperam');
+
+INSERT INTO passwords_data 
+(account_ID, password, time_of_creation, comment, is_current)
+VALUES (
+  LAST_INSERT_ID(),
+  AES_ENCRYPT('MyNewSnapPass2025!', UNHEX(SHA2('the dog in the field',512)), RANDOM_BYTES(16)),
+  '2025-10-08',
+  'Created new account entry for Snapchat site',
+  1
+);
