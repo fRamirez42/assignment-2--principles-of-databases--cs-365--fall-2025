1faa81494a45b038718d954695b3137efcad4e42
Foxyz24 <143655653+fRamirez42@users.noreply.github.com>
2025-10-08 15:47:23 -0400

    Fully worked out the new table for the iv_password store and then updated all commands

--- commands.sql
+++ commands.sql
@@ -1,132 +1,132 @@
 -- Command 1: Add a new entry --
-SET block_encryption_mode = 'aes-256-cbc';
-SET @key_str = UNHEX(SHA2('the dog in the field',512));
-SET @init_vector = RANDOM_BYTES(16);
+SET @url      = 'https://snapchat.com';
+SET @email    = 'felipe@snapchat.com';
+SET @username = 'feliperam';
+SET @plain    = 'MyNewSnapPass2025!';
+SET @date     = '2025-10-08';
+SET @comment  = 'Created new account entry for Snapchat site';
+SET @is_cur   = 1;
 
-INSERT INTO sites (url)
-VALUES ('https://snapchat.com');
+INSERT INTO sites(url)
+VALUES(@url)
+ON DUPLICATE KEY UPDATE site_ID = LAST_INSERT_ID(site_ID);
+SET @site_id := LAST_INSERT_ID();
 
-INSERT INTO accounts (site_ID, email, username)
-VALUES (LAST_INSERT_ID(), 'felipe@snapchat.com', 'feliperam');
+INSERT INTO accounts(site_ID,email,username)
+VALUES(@site_id,@email,@username)
+ON DUPLICATE KEY UPDATE account_ID = LAST_INSERT_ID(account_ID);
+SET @account_id := LAST_INSERT_ID();
 
-INSERT INTO passwords_data 
-(account_ID, password, time_of_creation, comment, is_current)
-VALUES (
-  LAST_INSERT_ID(),
-  AES_ENCRYPT('MyNewSnapPass2025!', @key_str, @innit_vector),
-  '2025-10-08',
-  'Created new account entry for Snapchat site',
-  1
-);
-INSERT INTO hex_number (hex_number)
-  VALUES(@innit_vector)
+SET @iv = RANDOM_BYTES(16);
+
+INSERT INTO passwords_data(account_ID,password,time_of_creation,comment,is_current)
+VALUES(@account_id, AES_ENCRYPT(@plain,@key_str,@iv), @date, @comment, @is_cur);
+
+INSERT INTO password_iv_store(pass_ID, iv_value)
+VALUES(LAST_INSERT_ID(), @iv);
+
+-- Receipt
+SELECT 'site_ID' AS what, @site_id AS value
+UNION ALL SELECT 'account_ID', @account_id
+UNION ALL SELECT 'inserted_password_rows', ROW_COUNT();
 
 -- Command 2: Get the password associated with a URL --
-SELECT s.url, a.email, a.username,
-       CONVERT(AES_DECRYPT(p.password, @key_str, @init_vector) USING utf8) AS plaintext_password
-FROM sites s
-JOIN accounts a ON a.site_ID = s.site_ID
-JOIN passwords_data p ON p.account_ID = a.account_ID
-WHERE s.url = 'https://github.com';
-
--- Command 3: Get all password associated data for two entries that hold HTTPS in their URL --
-SET block_encryption_mode = 'aes-256-cbc';
-SET @key_str = UNHEX(SHA2('the dog in the field',512));
-SET @init_vector = x'0123456789ABCDEF0123456789ABCDEF';
+SET @lookup_url = 'https://github.com';
 
 SELECT 
-  s.url,
-  a.email,
-  a.username,
-  CONVERT(AES_DECRYPT(p.password, @key_str, @init_vector) USING utf8) AS decrypted_password,
-  p.time_of_creation,
-  p.comment,
-  p.is_current
-FROM sites s
-JOIN accounts a ON a.site_ID = s.site_ID
-JOIN passwords_data p ON p.account_ID = a.account_ID
+  s.url, a.email, a.username,
+  CONVERT(AES_DECRYPT(p.password, @key_str, iv.iv_value) USING utf8) AS decrypted_password,
+  p.time_of_creation, p.comment, p.is_current, p.pass_ID
+FROM passwords_data p
+JOIN password_iv_store iv ON iv.pass_ID = p.pass_ID
+JOIN accounts a ON a.account_ID = p.account_ID
+JOIN sites s    ON s.site_ID   = a.site_ID
+WHERE s.url = @lookup_url;
+
+-- Command 3: Get all password associated data for two entries that hold HTTPS in their URL --
+SELECT 
+  s.url, a.email, a.username,
+  CONVERT(AES_DECRYPT(p.password, @key_str, iv.iv_value) USING utf8) AS decrypted_password,
+  p.time_of_creation, p.comment, p.is_current, p.pass_ID
+FROM passwords_data p
+JOIN password_iv_store iv ON iv.pass_ID = p.pass_ID
+JOIN accounts a ON a.account_ID = p.account_ID
+JOIN sites s    ON s.site_ID   = a.site_ID
 WHERE s.url LIKE 'https%'
 LIMIT 2;
 
 -- Command 4: Change the URL associated with one of the passwords --
 UPDATE sites
-SET url = 'https://snapchat.com'
-WHERE site_ID = 5;
--- To verify --
+SET url = 'https://meta.com'
+WHERE site_ID = 2;
+
 SELECT * FROM sites;
 
 -- Command 5: Change the password to any entry --
+SET @target_pass_id = 3;
+SET @new_plain      = 'MyNewSecureSnapPass2025!';
+SET @new_date       = '2025-10-08';
+
+SET @iv = RANDOM_BYTES(16);
+
 UPDATE passwords_data
-SET 
-  password = AES_ENCRYPT('MyNewSecureSnapPass2025!', @key_str, @init_vector),
-  time_of_creation = '2025-10-08'
-WHERE pass_ID = 3;
--- To verify --
+SET password = AES_ENCRYPT(@new_plain, @key_str, @iv),
+    time_of_creation = @new_date
+WHERE pass_ID = @target_pass_id;
+
+-- replace/update IV row (1:1 by PK), safe upsert
+INSERT INTO password_iv_store(pass_ID, iv_value)
+VALUES(@target_pass_id, @iv)
+ON DUPLICATE KEY UPDATE iv_value = VALUES(iv_value);
+
 SELECT 
-  pass_ID,
-  account_ID,
-  CONVERT(AES_DECRYPT(password, @key_str, @init_vector) USING utf8) AS decrypted_password,
-  time_of_creation,
-  comment,
-  is_current
-FROM passwords_data
-WHERE pass_ID = 3;
+  p.pass_ID, p.account_ID,
+  CONVERT(AES_DECRYPT(p.password, @key_str, iv.iv_value) USING utf8) AS decrypted_password,
+  p.time_of_creation, p.comment, p.is_current
+FROM passwords_data p
+JOIN password_iv_store iv ON iv.pass_ID = p.pass_ID
+WHERE p.pass_ID = @target_pass_id;
 
 -- Command 6: delete tuple based on URL --
 DELETE p
 FROM passwords_data p
 JOIN accounts a ON p.account_ID = a.account_ID
-JOIN sites s ON a.site_ID = s.site_ID
+JOIN sites s    ON a.site_ID   = s.site_ID
 WHERE s.url = 'http://steam.com';
 
--- To Verify --
 SELECT 
-  s.url,
-  a.email,
-  a.username,
-  p.pass_ID,
-  CONVERT(AES_DECRYPT(p.password, @key_str, @init_vector) USING utf8) AS decrypted_password,
-  p.time_of_creation,
-  p.comment,
-  p.is_current
+  s.url, a.email, a.username, p.pass_ID,
+  CONVERT(AES_DECRYPT(p.password, @key_str, iv.iv_value) USING utf8) AS decrypted_password,
+  p.time_of_creation, p.comment, p.is_current
 FROM passwords_data p
+JOIN password_iv_store iv ON iv.pass_ID = p.pass_ID
 JOIN accounts a ON p.account_ID = a.account_ID
-JOIN sites s ON a.site_ID = s.site_ID
+JOIN sites s    ON a.site_ID   = s.site_ID
 ORDER BY p.pass_ID;
 
 -- Command 7: delete tuple based on a password --
-SELECT a.account_ID, s.site_ID
-INTO @acc_id, @site_id
+SET @pwd_text = 'SchoolAppropriate78@';
+
+SELECT a.account_ID, s.site_ID INTO @acc_id, @site_id
 FROM passwords_data p
+JOIN password_iv_store iv ON iv.pass_ID = p.pass_ID
 JOIN accounts a ON p.account_ID = a.account_ID
-JOIN sites s ON a.site_ID = s.site_ID
-WHERE CONVERT(AES_DECRYPT(p.password, @key_str, @init_vector) USING utf8) = 'SchoolAppropriate78@'
+JOIN sites s    ON a.site_ID   = s.site_ID
+WHERE CONVERT(AES_DECRYPT(p.password, @key_str, iv.iv_value) USING utf8) = @pwd_text
 LIMIT 1;
 
--- delete the password row --
-DELETE FROM passwords_data
-WHERE account_ID = @acc_id;
-
--- delete the account row --
-DELETE FROM accounts
-WHERE account_ID = @acc_id;
-
--- delete the site row (only if no other accounts use it) --
+DELETE FROM passwords_data WHERE account_ID = @acc_id;
+DELETE FROM accounts       WHERE account_ID = @acc_id;
 DELETE FROM sites
 WHERE site_ID = @site_id
-  AND site_ID NOT IN (SELECT site_ID FROM accounts);
+  AND NOT EXISTS (SELECT 1 FROM accounts WHERE site_ID = @site_id);
 
--- To verify --
 SELECT 
-  s.url,
-  a.email,
-  a.username,
-  p.pass_ID,
-  CONVERT(AES_DECRYPT(p.password, @key_str, @init_vector) USING utf8) AS decrypted_password,
-  p.time_of_creation,
-  p.comment,
-  p.is_current
+  s.url, a.email, a.username, p.pass_ID,
+  CONVERT(AES_DECRYPT(p.password, @key_str, iv.iv_value) USING utf8) AS decrypted_password,
+  p.time_of_creation, p.comment, p.is_current
 FROM passwords_data p
+JOIN password_iv_store iv ON iv.pass_ID = p.pass_ID
 JOIN accounts a ON p.account_ID = a.account_ID
-JOIN sites s ON a.site_ID = s.site_ID
+JOIN sites s    ON a.site_ID   = s.site_ID
 ORDER BY p.pass_ID;
--- setup.sql
+++ setup.sql
@@ -4,89 +4,122 @@ USE passwords;
 
 SET block_encryption_mode = 'aes-256-cbc';
 SET @key_str = UNHEX(SHA2('the dog in the field', 512));
-SET @init_vector = RANDOM_BYTES(16); -- Create a secondary table --
 
 CREATE TABLE IF NOT EXISTS sites (
-  site_ID     INT UNSIGNED  NOT NULL AUTO_INCREMENT,
-  url         VARCHAR(512)  NOT NULL,
-
+  site_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  url     VARCHAR(512) NOT NULL,
   PRIMARY KEY (site_ID),
   UNIQUE KEY unique_site_url (url)
 );
 
 CREATE TABLE IF NOT EXISTS accounts (
-  site_ID     INT UNSIGNED  NOT NULL,
-  account_ID  INT UNSIGNED  NOT NULL AUTO_INCREMENT,
-  email       VARCHAR(256)  NOT NULL,
-  username    VARCHAR(256)  NOT NULL,
-
-  PRIMARY KEY(account_ID)
+  account_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  site_ID    INT UNSIGNED NOT NULL,
+  email      VARCHAR(256) NOT NULL,
+  username   VARCHAR(256) NOT NULL,
+  PRIMARY KEY (account_ID)
 );
 
 CREATE TABLE IF NOT EXISTS passwords_data (
-  pass_ID           INT UNSIGNED    NOT NULL AUTO_INCREMENT,
-  account_ID        INT UNSIGNED    NOT NULL,
-  password          VARBINARY(256)  NOT NULL,
-  time_of_creation  DATE            NOT NULL DEFAULT (CURRENT_DATE),
-  comment           VARCHAR(256)    NULL,
-  is_current        TINYINT(1)      NOT NULL,
-
-  PRIMARY KEY(pass_ID)
+  pass_ID          INT UNSIGNED   NOT NULL AUTO_INCREMENT,
+  account_ID       INT UNSIGNED   NOT NULL,
+  password         VARBINARY(256) NOT NULL,
+  time_of_creation DATE           NOT NULL DEFAULT (CURRENT_DATE),
+  comment          VARCHAR(256)   NULL,
+  is_current       TINYINT(1)     NOT NULL,
+  PRIMARY KEY (pass_ID)
 );
 
-CREATE TABLE IF NOT EXISTS hex_vectors(
-  pass_ID     INT UNSIGNED  NOT NULL AUTO_INCREMENT,
-  hex_number  VARCHAR(256)  NOT NULL,
+CREATE TABLE password_iv_store (
+  pass_ID   INT UNSIGNED NOT NULL,
+  iv_value  VARBINARY(16) NOT NULL,
+  PRIMARY KEY (pass_ID),
+  CONSTRAINT fk_iv_pass
+    FOREIGN KEY (pass_ID)
+    REFERENCES passwords_data(pass_ID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
 
-  PRIMARY KEY(pass_ID)
-)
-
-INSERT INTO sites VALUES
-    (1, "https://mail.google.com"),
-    (2, "http://facebook.com"),
-    (3, "https://github.com"),
-    (4, "https://youtube.com"),
-    (5, "http://steam.com"),
-    (6, "https://hartford.edu");
+INSERT INTO sites (site_ID, url) VALUES
+  (1, 'https://mail.google.com'),
+  (2, 'http://facebook.com'),
+  (3, 'https://github.com'),
+  (4, 'https://youtube.com'),
+  (5, 'http://steam.com'),
+  (6, 'https://hartford.edu');
 
 INSERT INTO accounts (site_ID, email, username) VALUES
-  (1, "theshowman@gmail.com", "theshowman"),
-  (1, "feliperam1990@gmail.com", "feliperam"),
-  (2, "feliperam1990@gmail.com", "feliper44"),
-  (3, "felipeprofessional@gmail.com", "felipe24"),
-  (4, "pipesuper24@gmail.com", "NextBigThing99+"),
-  (5, "feliperam1990@gmail.com", "WeBall1234"),
-  (5, "theshowman@gmail.com", "TheShowManIsHere!"),
-  (6, "jdoe@hartford.edu", "jdoe");
+  (1, 'theshowman@gmail.com', 'theshowman'),
+  (1, 'feliperam1990@gmail.com', 'feliperam'),
+  (2, 'feliperam1990@gmail.com', 'feliper44'),
+  (3, 'felipeprofessional@gmail.com', 'felipe24'),
+  (4, 'pipesuper24@gmail.com', 'NextBigThing99+'),
+  (5, 'feliperam1990@gmail.com', 'WeBall1234'),
+  (5, 'theshowman@gmail.com', 'TheShowManIsHere!'),
+  (6, 'jdoe@hartford.edu', 'jdoe');
 
+SET @iv = RANDOM_BYTES(16);
+INSERT INTO passwords_data (account_ID, password, time_of_creation, comment, is_current)
+  VALUES (1, AES_ENCRYPT('HughJackman1234', @key_str, @iv), '2025-10-06', NULL, 1);
+INSERT INTO password_iv_store (pass_ID, iv_value) VALUES (LAST_INSERT_ID(), @iv);
 
-INSERT INTO passwords_data
-  (account_ID, password, time_of_creation, comment, is_current) VALUES
-  (1, AES_ENCRYPT('HughJackman1234', @key_str, @init_vector), '2025-10-06', NULL, 1),
-  (2, AES_ENCRYPT('SuperMario123', @key_str, @init_vector), '2010-06-24', 'Childish, and forgot', 0),
-  (2, AES_ENCRYPT('ProfessionalPassword26$',@key_str, @init_vector), '2022-02-27', NULL, 1),
-  (3, AES_ENCRYPT('GoldenRetriver56!', @key_str, @init_vector), '2012-04-29', 'Got Hacked', 0),
-  (3, AES_ENCRYPT('ThaWorldo98&', @key_str, @init_vector), '2017-01-31', NULL, 1),
-  (4, AES_ENCRYPT('FelipeRamirez9900*', @key_str, @init_vector), '2018-09-30', NULL, 1),
-  (5, AES_ENCRYPT('DaBoss67', @key_str, @init_vector), '2014-11-08', 'Forgot it', 0),
-  (5, AES_ENCRYPT('RemeberThisTime11#', @key_str, @init_vector), '2017-12-20', 'Forgot it again', 0),
-  (5, AES_ENCRYPT('DogGolden420@', @key_str, @init_vector), '2020-03-15', NULL, 0),
-  (6, AES_ENCRYPT('ShowmanshipIsKey12', @key_str, @init_vector), '2014-11-08', 'It is!', 1),
-  (6, AES_ENCRYPT('Dexter8877%', @key_str, @init_vector), '2023-08-20', NULL, 1),
-  (7, AES_ENCRYPT('IAmHere@', @key_str, @init_vector), '2022-08-20', NULL, 1),
-  (8, AES_ENCRYPT('SchoolAppropriate78@', @key_str, @init_vector), '2020-12-31', NULL, 1);
+SET @iv = RANDOM_BYTES(16);
+INSERT INTO passwords_data (account_ID, password, time_of_creation, comment, is_current)
+  VALUES (2, AES_ENCRYPT('SuperMario123', @key_str, @iv), '2010-06-24', 'Childish, and forgot', 0);
+INSERT INTO password_iv_store (pass_ID, iv_value) VALUES (LAST_INSERT_ID(), @iv);
 
-INSERT INTO hex_vectors
-  (1, @init_vector),
-  (2, @init_vector),
-  (3, @init_vector),
-  (4, @init_vector),
-  (5, @init_vector),
-  (6, @init_vector),
-  (7, @init_vector),
-  (8, @init_vector),
-  (9, @init_vector),
-  (10, @init_vector),
-  (11, @init_vector),
-  (12, @init_vector),
-  (13, @init_vector);
+SET @iv = RANDOM_BYTES(16);
+INSERT INTO passwords_data (account_ID, password, time_of_creation, comment, is_current)
+  VALUES (2, AES_ENCRYPT('ProfessionalPassword26$', @key_str, @iv), '2022-02-27', NULL, 1);
+INSERT INTO password_iv_store (pass_ID, iv_value) VALUES (LAST_INSERT_ID(), @iv);
+
+SET @iv = RANDOM_BYTES(16);
+INSERT INTO passwords_data (account_ID, password, time_of_creation, comment, is_current)
+  VALUES (3, AES_ENCRYPT('GoldenRetriver56!', @key_str, @iv), '2012-04-29', 'Got Hacked', 0);
+INSERT INTO password_iv_store (pass_ID, iv_value) VALUES (LAST_INSERT_ID(), @iv);
+
+SET @iv = RANDOM_BYTES(16);
+INSERT INTO passwords_data (account_ID, password, time_of_creation, comment, is_current)
+  VALUES (3, AES_ENCRYPT('ThaWorldo98&', @key_str, @iv), '2017-01-31', NULL, 1);
+INSERT INTO password_iv_store (pass_ID, iv_value) VALUES (LAST_INSERT_ID(), @iv);
+
+SET @iv = RANDOM_BYTES(16);
+INSERT INTO passwords_data (account_ID, password, time_of_creation, comment, is_current)
+  VALUES (4, AES_ENCRYPT('FelipeRamirez9900*', @key_str, @iv), '2018-09-30', NULL, 1);
+INSERT INTO password_iv_store (pass_ID, iv_value) VALUES (LAST_INSERT_ID(), @iv);
+
+SET @iv = RANDOM_BYTES(16);
+INSERT INTO passwords_data (account_ID, password, time_of_creation, comment, is_current)
+  VALUES (5, AES_ENCRYPT('DaBoss67', @key_str, @iv), '2014-11-08', 'Forgot it', 0);
+INSERT INTO password_iv_store (pass_ID, iv_value) VALUES (LAST_INSERT_ID(), @iv);
+
+SET @iv = RANDOM_BYTES(16);
+INSERT INTO passwords_data (account_ID, password, time_of_creation, comment, is_current)
+  VALUES (5, AES_ENCRYPT('RemeberThisTime11#', @key_str, @iv), '2017-12-20', 'Forgot it again', 0);
+INSERT INTO password_iv_store (pass_ID, iv_value) VALUES (LAST_INSERT_ID(), @iv);
+
+SET @iv = RANDOM_BYTES(16);
+INSERT INTO passwords_data (account_ID, password, time_of_creation, comment, is_current)
+  VALUES (5, AES_ENCRYPT('DogGolden420@', @key_str, @iv), '2020-03-15', NULL, 0);
+INSERT INTO password_iv_store (pass_ID, iv_value) VALUES (LAST_INSERT_ID(), @iv);
+
+SET @iv = RANDOM_BYTES(16);
+INSERT INTO passwords_data (account_ID, password, time_of_creation, comment, is_current)
+  VALUES (6, AES_ENCRYPT('ShowmanshipIsKey12', @key_str, @iv), '2014-11-08', 'It is!', 1);
+INSERT INTO password_iv_store (pass_ID, iv_value) VALUES (LAST_INSERT_ID(), @iv);
+
+SET @iv = RANDOM_BYTES(16);
+INSERT INTO passwords_data (account_ID, password, time_of_creation, comment, is_current)
+  VALUES (6, AES_ENCRYPT('Dexter8877%', @key_str, @iv), '2023-08-20', NULL, 1);
+INSERT INTO password_iv_store (pass_ID, iv_value) VALUES (LAST_INSERT_ID(), @iv);
+
+SET @iv = RANDOM_BYTES(16);
+INSERT INTO passwords_data (account_ID, password, time_of_creation, comment, is_current)
+  VALUES (7, AES_ENCRYPT('IAmHere@', @key_str, @iv), '2022-08-20', NULL, 1);
+INSERT INTO password_iv_store (pass_ID, iv_value) VALUES (LAST_INSERT_ID(), @iv);
+
+SET @iv = RANDOM_BYTES(16);
+INSERT INTO passwords_data (account_ID, password, time_of_creation, comment, is_current)
+  VALUES (8, AES_ENCRYPT('SchoolAppropriate78@', @key_str, @iv), '2020-12-31', NULL, 1);
+INSERT INTO password_iv_store (pass_ID, iv_value) VALUES (LAST_INSERT_ID(), @iv);
